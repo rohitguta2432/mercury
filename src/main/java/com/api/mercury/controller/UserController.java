24410ba458c03ea95ef3ee4d8629262aaccf947e
rohit raj <rohitgupta2432@gmail.com>
2020-02-19 18:42:10 +0530

    swagger integration

--- src/main/java/com/api/mercury/controller/UserController.java
+++ src/main/java/com/api/mercury/controller/UserController.java
@@ -6,10 +6,9 @@ import com.api.mercury.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("user")
@@ -18,12 +17,13 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    /*@GetMapping
-    public ResponseEntity<Response<List<UserDTO>>> getAllUser() {
-        return new ResponseEntity<>(new Response<>(HttpStatus.OK.value(), "user fetched successfully", userService.getAllUsers()), HttpStatus.OK);
-    }*/
     @PostMapping
     public ResponseEntity<Response<UserDTO>> save(@RequestBody UserDTO userDTO) {
         return new ResponseEntity<>(new Response<>(HttpStatus.OK.value(), "user saved successfully", userService.save(userDTO)), HttpStatus.OK);
+    }
+
+    @PostMapping("{id}")
+    public ResponseEntity<Response<UserDTO>> getUserById(@PathVariable UUID id) {
+        return new ResponseEntity<>(new Response<>(HttpStatus.OK.value(), "user fetched successfully", userService.get(id)), HttpStatus.OK);
     }
 }
--- src/main/java/com/api/mercury/service/UserService.java
+++ src/main/java/com/api/mercury/service/UserService.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -16,13 +16,13 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-   /* public List<UserDTO> getAllUsers() {
-        log.info("fetching all users ");
-        return (List<UserDTO>) UserMapper.INSTANCE.userListToUserDTO(userRepository.findAll());
-    }*/
 
     public UserDTO save(UserDTO userDTO) {
         log.info("save user");
         return UserMapper.INSTANCE.userToUserDTO(userRepository.save(UserMapper.INSTANCE.UserDtoToUser(userDTO)));
+    }
+    public UserDTO get(UUID id){
+        log.info("fetch user by id");
+        return UserMapper.INSTANCE.userToUserDTO(userRepository.findById(id).get());
     }
 }
